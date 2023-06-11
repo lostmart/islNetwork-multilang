bd5f93435341a5c9b0dc00703f3bc29f891ca0cb
lostmart <lostmart@hotmail.com>
2023-06-11 10:55:45 +0200

    navBar translated

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,66 +1,107 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue'
-import { useStore } from 'vuex'
+	import { ref, onMounted, watch } from 'vue'
+	import { useStore } from 'vuex'
 
-import ButtonGroup from './ButtonGroup.vue'
+	import ButtonGroup from './ButtonGroup.vue'
 
-/*  locales  */
-import engTxt from '../locale/en.json'
-import spanishTxt from '../locale/sp.json'
+	/*  locales  */
+	import engTxt from '../locale/en.json'
+	import spanishTxt from '../locale/sp.json'
 
-const store = useStore()
+	const store = useStore()
 
-const NavBar = ref({ home: '', about: '' })
+	const NavBarData = ref({
+		home: '',
+		about: '',
+		team: '',
+		faq: '',
+		voices: '',
+		materials: '',
+		contact: '',
+	})
 
-// Watch for changes in the store state siteLang
-watch(
-	() => store.state.siteLang,
-	() => {
+	// Watch for changes in the store state siteLang
+	watch(
+		() => store.state.siteLang,
+		() => {
+			setPageLang()
+		}
+	)
+
+	/**
+	 * Sets the page language based on the value stored in localStorage.
+	 * If the language is set to 'sp', => Spanish
+	 * If the language is set to 'en', => English
+	 * @returns {void}
+	 */
+	const setPageLang = () => {
+		if (localStorage.getItem('lang') === 'sp') {
+			NavBarData.value = spanishTxt.nav
+		} else if (localStorage.getItem('lang') === 'en') {
+			NavBarData.value = engTxt.nav
+		}
+		console.log(NavBarData.value.home)
+	}
+
+	onMounted(() => {
 		setPageLang()
-	}
-)
-
-
-
-
-/**
- * Sets the page language based on the value stored in localStorage.
- * If the language is set to 'sp', => Spanish
- * If the language is set to 'en', => English
- * @returns {void}
- */
-const setPageLang = () => {
-	if (localStorage.getItem('lang') === 'sp') {
-		NavBar.value.home = spanishTxt.nav.home
-		NavBar.value.about = spanishTxt.nav.about
-	} else if (localStorage.getItem('lang') === 'en') {
-		NavBar.value.home = engTxt.nav.home
-		NavBar.value.about = engTxt.nav.about
-	}
-}
-
-onMounted(() => {
-	setPageLang()
-})
+	})
 </script>
 
 <template>
 	<nav class="navbar navbar-expand-lg navbar-light bg-primary">
 		<div class="container">
 			<router-link to="/" class="navbar-brand text-white">
-				<img src="../assets/images/favicon-32x32.png" alt="padrinos sin limites" />
+				<img
+					src="../assets/images/favicon-32x32.png"
+					alt="padrinos sin limites" />
 			</router-link>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<router-link class="nav-link text-white" aria-current="page" to="/">{{
-						NavBar.home }}
+					<router-link class="nav-link text-white" aria-current="page" to="/"
+						>{{ NavBarData.home }}
 					</router-link>
 					<li class="nav-item">
-						<router-link class="nav-link text-white" to="/about">{{ NavBar.about }}</router-link>
+						<router-link class="nav-link text-white" to="/about">{{
+							NavBarData.about
+						}}</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link class="nav-link text-white" to="/team">{{
+							NavBarData.team
+						}}</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link class="nav-link text-white" to="/faq">{{
+							NavBarData.faq
+						}}</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link class="nav-link text-white" to="/voices">{{
+							NavBarData.voices
+						}}</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link class="nav-link text-white" to="/materials">{{
+							NavBarData.materials
+						}}</router-link>
+					</li>
+					<li class="nav-item">
+						<a
+							class="nav-link text-white"
+							href="mailto: ruralgodparents@gmail.com"
+							>{{ NavBarData.contact }}</a
+						>
 					</li>
 				</ul>
 				<ButtonGroup />
