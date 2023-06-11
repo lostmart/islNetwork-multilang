50b7c2f01636810130a027794418a1928df11284
lostmart <lostmart@hotmail.com>
2023-06-11 12:05:44 +0200

    card and container as wrappers in the app.vue

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -1,7 +1,22 @@
-<script setup></script>
+<script setup>
+	import { ref, onMounted, watch } from 'vue'
+	import { useStore } from 'vuex'
+
+	/*  locales  */
+	import engTxt from '../locale/en.json'
+	import spanishTxt from '../locale/sp.json'
+
+	const store = useStore()
+	const teamData = ref({ title: '', subTitle: '', paragraphs: [] })
+</script>
 
 <template>
 	<div>
-		<h2>he hi !</h2>
+		<h2 class="card-title mt-4">Nuestro equipo</h2>
+		<h3 class="card-title text-center">COORDINACIÓN</h3>
+		<img
+			class="img-laura img-thumbnail"
+			src="../assets/images/laura-motta.jpg"
+			alt="Laura Motta coordinadora" />
 	</div>
 </template>
