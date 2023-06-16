0e671005768c6c2d3a565ead2d086dae179e2015
lostmart <martinpedraza1979@hmail.com>
2023-06-16 20:22:46 +0200

    styles and coordiniates

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -1,54 +1,49 @@
 <script setup>
-	import { ref, onMounted, watch } from 'vue'
-	import { useStore } from 'vuex'
+import { ref, onMounted, watch } from 'vue'
+import { useStore } from 'vuex'
 
-	import TeamMember from '../components/TeamMember.vue'
+import TeamMember from '../components/TeamMember.vue'
 
-	/*  locales  */
-	import engTxt from '../locale/en.json'
-	import spanishTxt from '../locale/sp.json'
+/*  locales  */
+import engTxt from '../locale/en.json'
+import spanishTxt from '../locale/sp.json'
 
-	const store = useStore()
-	const teamData = ref({ title: '', teamMembers: [] })
+const store = useStore()
+const teamData = ref({ title: '', teamMembers: [] })
 
-	// Watch for changes in the store state siteLang
-	watch(
-		() => store.state.siteLang,
-		() => {
-			setPageLang()
-		}
-	)
-
-	/**
-	 * Sets the page language based on the value stored in localStorage.
-	 * If the language is set to 'sp', => Spanish
-	 * If the language is set to 'en', => English
-	 * @returns {void}
-	 */
-	const setPageLang = () => {
-		if (localStorage.getItem('lang') === 'sp') {
-			teamData.value = spanishTxt.team
-		} else if (localStorage.getItem('lang') === 'en') {
-			teamData.value = engTxt.team
-		}
-	}
-
-	onMounted(() => {
+// Watch for changes in the store state siteLang
+watch(
+	() => store.state.siteLang,
+	() => {
 		setPageLang()
-	})
+	}
+)
+
+/**
+ * Sets the page language based on the value stored in localStorage.
+ * If the language is set to 'sp', => Spanish
+ * If the language is set to 'en', => English
+ * @returns {void}
+ */
+const setPageLang = () => {
+	if (localStorage.getItem('lang') === 'sp') {
+		teamData.value = spanishTxt.team
+	} else if (localStorage.getItem('lang') === 'en') {
+		teamData.value = engTxt.team
+	}
+}
+
+onMounted(() => {
+	setPageLang()
+})
 </script>
 
 <template>
 	<div class="row container">
 		<h2 class="card-title mt-4">{{ teamData.title }}</h2>
-		<div class="col-md-6 col-lg-4" v-for="member in teamData.teamMembers">
-			<TeamMember
-				class="m-4"
-				:title="member.userName"
-				:description="member.description"
-				:imgUrl="member.imgUrl"
-				:linkedIn="member.linkedIn"
-				:role="member.rol" />
+		<div class="col-md-6 col-lg-4" v-for="member in teamData.teamMembers" :key="member.userName">
+			<TeamMember class="m-4" :title="member.userName" :description="member.description" :imgUrl="member.imgUrl"
+				:linkedIn="member.linkedIn" :rol="member.rol" />
 		</div>
 	</div>
 </template>
