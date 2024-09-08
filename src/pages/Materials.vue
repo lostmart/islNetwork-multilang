fbe583f0e1b2fbd862ddb79b3d52ffa705c28ac6
lostmart <martinpedraza1979@gmail.com>
2024-09-08 10:33:29 +0200

    material de teatro

--- src/pages/Materials.vue
+++ src/pages/Materials.vue
@@ -1,40 +1,40 @@
 <script setup>
-	import { ref, onMounted, watch } from 'vue'
-	import { useStore } from 'vuex'
+import { ref, onMounted, watch } from 'vue'
+import { useStore } from 'vuex'
 
-	/*  locales  */
-	import engTxt from '../locale/en.json'
-	import spanishTxt from '../locale/sp.json'
+/*  locales  */
+import engTxt from '../locale/en.json'
+import spanishTxt from '../locale/sp.json'
 
-	const materials = ref(null)
-	const btnTxt = ref(null)
-	const store = useStore()
+const materials = ref(null)
+const btnTxt = ref(null)
+const store = useStore()
 
-	// Watch for changes in the store state siteLang
-	watch(
-		() => store.state.siteLang,
-		() => {
-			setPageLang()
-		}
-	)
-
-	/**
-	 * Sets the page language based on the value stored in localStorage.
-	 * If the language is set to 'sp', => Spanish
-	 * If the language is set to 'en', => English
-	 * @returns {void}
-	 */
-	const setPageLang = () => {
-		if (localStorage.getItem('lang') === 'sp') {
-			materials.value = spanishTxt.materials
-		} else if (localStorage.getItem('lang') === 'en') {
-			materials.value = engTxt.materials
-		}
-	}
-
-	onMounted(() => {
+// Watch for changes in the store state siteLang
+watch(
+	() => store.state.siteLang,
+	() => {
 		setPageLang()
-	})
+	}
+)
+
+/**
+ * Sets the page language based on the value stored in localStorage.
+ * If the language is set to 'sp', => Spanish
+ * If the language is set to 'en', => English
+ * @returns {void}
+ */
+const setPageLang = () => {
+	if (localStorage.getItem('lang') === 'sp') {
+		materials.value = spanishTxt.materials
+	} else if (localStorage.getItem('lang') === 'en') {
+		materials.value = engTxt.materials
+	}
+}
+
+onMounted(() => {
+	setPageLang()
+})
 </script>
 
 <template>
@@ -42,35 +42,19 @@
 		<h2 class="text-center mt-4">{{ materials.title }}</h2>
 		<div v-if="show" class="p-4">
 			<div class="list-group mx-auto">
-				<a
-					target="_blank"
-					v-for="book in books"
-					:key="book.link"
-					:href="book.link"
-					class="list-group-item list-group-item-action text-center"
-					aria-current="true">
+				<a target="_blank" v-for="book in books" :key="book.link" :href="book.link"
+					class="list-group-item list-group-item-action text-center" aria-current="true">
 					{{ book.title }}
 				</a>
 			</div>
 		</div>
 		<div v-else>
 			<h3 class="text-center mt-4">{{ materials.subTitle }}</h3>
-			<form
-				ref="formPass"
-				class="mx-auto bg-primary p-4"
-				@submit.prevent="handleSubmit">
+			<form ref="formPass" class="mx-auto bg-primary p-4" @submit.prevent="handleSubmit">
 				<label for="inputPassword" class="form-label mb-3">Password</label>
 				<div class="input-group mb-3">
-					<input
-						:type="inputType"
-						class="form-control"
-						id="inputPassword"
-						v-model="input" />
-					<button
-						class="btn btn-light"
-						type="button"
-						id="button-addon2"
-						@click="togglePassVis">
+					<input :type="inputType" class="form-control" id="inputPassword" v-model="input" />
+					<button class="btn btn-light" type="button" id="button-addon2" @click="togglePassVis">
 						<i v-if="!passShw" class="far fa-eye-slash"></i>
 						<i v-else class="far fa-eye"></i>
 					</button>
@@ -82,111 +66,121 @@
 </template>
 
 <script>
-	export default {
-		name: 'materials',
-		data() {
-			return {
-				title: 'ISL network | materiales',
-				books: [
-					{
-						title: 'Libro Uno',
-						link: 'https://www.dropbox.com/s/gbklb3ue6m6tj1q/ISL%20LIBRO%201.pdf?dl=0',
-					},
-					{
-						title: 'Libro Dos',
-						link: 'https://www.dropbox.com/s/w5jz532z4c5m600/ISL%20BOOK%202.pdf?dl=0',
-					},
-					{
-						title: 'Flashcards',
-						link: 'https://www.dropbox.com/s/muz5zyfwxq9dme5/Flashcards%20ISL%20A%C3%91O%201%202022.pdf?dl=0',
-					},
-					{
-						title: 'Flashcards II',
-						link: 'https://www.dropbox.com/s/pip7qlpfksthqy5/ISL%20Flashcards%20A%C3%91O%202.pdf?dl=0',
-					},
-					{
-						title: 'Youtube playlist',
-						link: 'https://www.youtube.com/playlist?list=PLtupDLnojS6s_LeuiI141CCc8fctt1fgZ',
-					},
-					{
-						title: 'Otros / Misc',
-						link: 'https://drive.google.com/drive/folders/1q38CqBHNcmWGmfHCpgXHYWIfWtfFnlhH',
-					},
-				],
-				cogin: 'Gdp2019',
-				show: false,
-				input: '',
-				passShw: false,
-			}
+export default {
+	name: 'materials',
+	data() {
+		return {
+			title: 'ISL network | materiales',
+			books: [
+				{
+					title: 'Libro Uno',
+					link: 'https://www.dropbox.com/s/gbklb3ue6m6tj1q/ISL%20LIBRO%201.pdf?dl=0',
+				},
+				{
+					title: 'Libro Dos',
+					link: 'https://www.dropbox.com/s/w5jz532z4c5m600/ISL%20BOOK%202.pdf?dl=0',
+				},
+				{
+					title: 'Flashcards',
+					link: 'https://www.dropbox.com/s/muz5zyfwxq9dme5/Flashcards%20ISL%20A%C3%91O%201%202022.pdf?dl=0',
+				},
+				{
+					title: 'Flashcards II',
+					link: 'https://www.dropbox.com/s/pip7qlpfksthqy5/ISL%20Flashcards%20A%C3%91O%202.pdf?dl=0',
+				},
+				{
+					title: 'Youtube playlist',
+					link: 'https://www.youtube.com/playlist?list=PLtupDLnojS6s_LeuiI141CCc8fctt1fgZ',
+				},
+				{
+					title: 'Otros / Misc',
+					link: 'https://drive.google.com/drive/folders/1q38CqBHNcmWGmfHCpgXHYWIfWtfFnlhH',
+				},
+				{
+					title: 'Material de Teatro',
+					link: 'https://drive.google.com/drive/folders/11Boynko1i_awmNOWfoahLERCmAatkkJH?usp=sharing'
+				}
+			],
+			cogin: 'Gdp2019',
+			show: false,
+			input: '',
+			passShw: false,
+		}
+	},
+	methods: {
+		handleSubmit() {
+			this.input === this.cogin ? (this.show = true) : this.handleError()
 		},
-		methods: {
-			handleSubmit() {
-				this.input === this.cogin ? (this.show = true) : this.handleError()
-			},
-			handleError() {
-				this.$refs.formPass.classList.add('shake')
-				setTimeout(() => {
-					this.$refs.formPass.classList.remove('shake')
-					this.input = ''
-				}, 450)
-			},
-			togglePassVis() {
-				this.passShw = !this.passShw
-			},
+		handleError() {
+			this.$refs.formPass.classList.add('shake')
+			setTimeout(() => {
+				this.$refs.formPass.classList.remove('shake')
+				this.input = ''
+			}, 450)
 		},
-		computed: {
-			inputType: function () {
-				if (!this.passShw) return 'password'
-				else return 'text'
-			},
+		togglePassVis() {
+			this.passShw = !this.passShw
 		},
-	}
+	},
+	computed: {
+		inputType: function () {
+			if (!this.passShw) return 'password'
+			else return 'text'
+		},
+	},
+}
 </script>
 
 <style scoped>
-	form {
-		color: white;
-		max-width: 500px;
+form {
+	color: white;
+	max-width: 500px;
+}
+
+.list-group {
+	max-width: 500px;
+}
+
+.list-group-item {
+	color: #418bd0;
+}
+
+.bg-primary {
+	background-color: #418bd0 !important;
+	border-radius: 0.375rem;
+}
+
+.btn-secondary {
+	background-color: #f09f06 !important;
+}
+
+a {
+	text-decoration: none;
+}
+
+.shake {
+	animation: shake 0.17s infinite;
+}
+
+@keyframes shake {
+	0% {
+		transform: translateX(0);
 	}
 
-	.list-group {
-		max-width: 500px;
-	}
-	.list-group-item {
-		color: #418bd0;
-	}
-	.bg-primary {
-		background-color: #418bd0 !important;
-		border-radius: 0.375rem;
+	25% {
+		transform: translateX(-15px);
 	}
 
-	.btn-secondary {
-		background-color: #f09f06 !important;
+	50% {
+		transform: translateX(0);
 	}
 
-	a {
-		text-decoration: none;
+	75% {
+		transform: translateX(10px);
 	}
 
-	.shake {
-		animation: shake 0.17s infinite;
+	100% {
+		transform: translateX(0);
 	}
-
-	@keyframes shake {
-		0% {
-			transform: translateX(0);
-		}
-		25% {
-			transform: translateX(-15px);
-		}
-		50% {
-			transform: translateX(0);
-		}
-		75% {
-			transform: translateX(10px);
-		}
-		100% {
-			transform: translateX(0);
-		}
-	}
+}
 </style>
